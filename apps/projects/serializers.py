cc25a1f4d8df14072a511899ef74195d2988c572
Matokhin <matokhin_aleksey@mail.ru>
2026-07-27 23:33:40 +0300

    implement projectviewset

--- apps/projects/serializers.py
+++ apps/projects/serializers.py
@@ -2,9 +2,7 @@ from rest_framework import serializers
 from .models import Project
 
 class ProjectSerializer(serializers.ModelSerializer):
-    owner = serializers.HiddenField(default = serializers.CurrentUserDefault())
-
     class Meta:
         model = Project
         fields = ('id', 'title', 'description', 'owner', 'created_at', 'updated_at')
-        read_only_fields = ('id', 'created_at', 'updated_at')
+        read_only_fields = ('id', 'owner', 'created_at', 'updated_at')

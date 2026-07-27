2e022b4ddc169ec35eaf4d0353992dc288a01a2c
Matokhin <matokhin_aleksey@mail.ru>
2026-07-28 01:29:30 +0300

    add project routes

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('apps.users.urls')),
+    path('api/users/', include('apps.users.urls')),
+    path('api/projects/', include('apps.projects.urls')),
 ]

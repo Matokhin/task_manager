9bdc7331f737ca5054deeed26b8083166ef486a4
Matokhin <matokhin_aleksey@mail.ru>
2026-07-28 05:02:24 +0300

    add tasks routes

--- config/urls.py
+++ config/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('apps.users.urls')),
     path('api/projects/', include('apps.projects.urls')),
+    path('api/tasks/', include('apps.tasks.urls')),
 ]

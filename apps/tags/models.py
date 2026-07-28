e4f6681c1c0433ed9a4cf9edff292708b3fc05f1
Matokhin <matokhin_aleksey@mail.ru>
2026-07-28 06:14:57 +0300

    add tag model

--- apps/tags/models.py
+++ apps/tags/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(
+        max_length=50,
+        unique=True,
+        verbose_name="Название тега"
+    )
+
+    class Meta:
+        verbose_name = "Тег"
+        verbose_name_plural = "Теги"
+
+    def __str__(self):
+        return self.name

38c9f1d0e17d706304eea1ea4c686560414e5763
Matokhin <matokhin_aleksey@mail.ru>
2026-07-28 06:23:57 +0300

    add comment model

--- apps/comments/models.py
+++ apps/comments/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Comment(models.Model):
+    task = models.ForeignKey(
+        "tasks.Task",
+        on_delete=models.CASCADE,
+        related_name="comments",
+    )
+
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="comments",
+    )
+
+    text = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
